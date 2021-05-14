c8807ba90ef7ebf46da5bdbd16e9b42b0106a22c
JordiE26 <84114976+JordiE26@users.noreply.github.com>
2021-05-13 21:36:56 -0500

    Add files via upload

---
+++ Nodo_de_datos_2/Nodo_de_datos_2.ino
@@ -0,0 +1,120 @@
+
+/* UNIVERSIDAD LAICA “ELOY ALFARO” DE MANABÍ
+   FACULTAD DE CIENCIAS INFORMÁTICAS
+                TEMA: 
+“PROTOTIPO Y DISEÑO DE UNA RED DE SENSORES INALÁMBRICOS
+    PARA EL CONTROL  DE UN HUERTO UNIFAMILIAR 
+         POR MEDIO DE UN APLICATIVO WEB, 
+ EN LA PARROQUIA ELOY ALFARO DE LA CIUDAD DE MANTA.”
+                AUTORES:
+Sr. Estupiñan Rosero Jordi Ariel
+Sr. García Choez Jorge Enrique */
+
+/*CONFIGURACION NODO DE DATOS #2 SENSOR DE HUMEDAD DEL SUELO (FC-28)*/
+
+#include <espnow.h>
+#include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+
+// Definimos las ID del nodo de datos 2 (ESP32 nodo de datos #1 = DATOS 1, ESP32 nodo de datos #2 = DATOS 2)
+#define NODO_DATOS 2
+
+#define hum_suelo A0
+
+//Direccion MAC del receptor (Nodo coordinador) 
+uint8_t broadcastAddress[] = {0x98, 0xF4, 0xAB, 0xDD, 0x06, 0x89};
+
+//Estructura para enviar datos
+//Debe coincidir con la estructura del receptor (Nodo coordinador)
+typedef struct struct_message {
+    int id;
+    float temp;
+    float hum;
+    float humS;
+    int readingId;
+} struct_message;
+
+//Crea un struct_message llamado myData
+struct_message myData;
+
+unsigned long previousMillis = 0;   // Almacena la última vez que se publico la temperatura y humedad
+const long interval = 10000;        // Intervalo en el que publicar las lecturas del sensor
+unsigned int readingId = 0;         //Realiza un seguimiento del número de lecturas enviadas.
+
+// Inserta el nombre de la red a la que se va a conectar tu ESP 
+constexpr char WIFI_SSID[] = "GARCIA ESTUPIÑAN";
+
+//Buscamos la red para obtener el canal de comunicación
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+    for (uint8_t i=0; i<n; i++) {
+      if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+        return WiFi.channel(i);
+      }
+    }
+  }
+  return 0;
+}
+
+float humS = analogRead(hum_suelo)/10.24;
+
+
+// Devolución de llamada cuando se envían datos
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Estado del último envío de paquete: ");
+  if (sendStatus == 0){
+    Serial.println("Envio Exitoso");
+  }
+  else{
+    Serial.println("Envio Fallido");
+  }
+}
+ 
+void setup() {
+  //Iniciar monitor serie
+  Serial.begin(115200);
+  
+
+  // Establecer el dispositivo como una estación Wi-Fi y establecer el canal
+  WiFi.mode(WIFI_STA);
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); // Para verificar el numero de canal anterior
+  wifi_promiscuous_enable(1);
+  wifi_set_channel(channel);
+  wifi_promiscuous_enable(0);
+  WiFi.printDiag(Serial); // Para verificar el numero de canal despues
+  
+  // Iniciar ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error al iniciar el ESP");
+    return;
+  }
+
+  // Una vez inicializado el ESP con éxito, nos registramos para enviar
+  // Obtener el estado del paquete enviado
+   esp_now_register_send_cb(OnDataSent);
+
+   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+  esp_now_register_send_cb(OnDataSent);
+  
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+  }
+ 
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Guardar la última vez que se publicó una nueva lectura
+    previousMillis = currentMillis;
+    //Establecer valores para enviar
+    myData.id = NODO_DATOS;
+    myData.humS = analogRead(hum_suelo);
+    myData.readingId = readingId++;
+     
+    //Send message via ESP-NOW
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+     Serial.print("loop");
+  }
+ }
